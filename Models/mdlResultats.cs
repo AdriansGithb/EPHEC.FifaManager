4c01e405f08d8a418986e267761443b0b08cbc1f
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-04-14 23:02:01 +0200

    création class mdlResultats

---
+++ Models/mdlResultats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class mdlResultats
+    {
+        public int match_ID;
+        public DateTime date;
+        public string nom_EqpDom;
+        public string nom_EqpVisit;
+        public int goalsDom;
+        public int goalsVisit;
+        public string resultDom;
+        public string resultVisit;
+    }
+}
