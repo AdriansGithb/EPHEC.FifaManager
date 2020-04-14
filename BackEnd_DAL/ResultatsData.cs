18716287f8b582a28fb938f823c567e7e1af8111
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-04-14 22:55:02 +0200

    Fct SelectResultats dans ResultatsData

--- BackEnd_DAL/ResultatsData.cs
+++ BackEnd_DAL/ResultatsData.cs
@@ -8,6 +8,11 @@ namespace BackEnd_DAL
 {
     public class ResultatsData
     {
-
+        public List<SP_SelectResults_Result> SelectResultats(int ssn)
+        {
+            Fifa_ManagerEntities ctx = new Fifa_ManagerEntities();
+            List<SP_SelectResults_Result> lstRes = ctx.SP_SelectResults(ssn).ToList();
+            return lstRes;
+        }
     }
 }
