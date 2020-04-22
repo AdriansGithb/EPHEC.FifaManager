5c9aa885e952c23d71e85224ea32337753c25f65
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-04-22 23:20:14 +0200

    BusinessError : +gestion erreur bd pas connectée

--- Errors/BusinessErrors.cs
+++ Errors/BusinessErrors.cs
@@ -17,7 +17,13 @@ namespace Errors
                 case "NewResultRecordInDB":
                    _Message = "Les données ont été modifiées entre-temps. Veuillez recommencer la procédure";
                     break;
-                default: _Message = "Erreur inconnue, veuillez contacter un administrateur";
+                case "Schema specified is not valid. Errors: \r\nFifaManager.ssdl(2,2) : error 0152: No Entity Framework provider found for the ADO.NET provider with invariant name 'System.Data.SqlClient'. Make sure the provider is registered in the 'entityFramework' section of the application config file. See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.":
+                    _Message = "La base de données n'est pas connectée. Contactez un administrateur.";
+                    break;
+                default: 
+                    _Message = "Erreur inconnue, veuillez contacter un administrateur";
+                //default:
+                //    _Message = erMsg;
                     break;
             }
         }
