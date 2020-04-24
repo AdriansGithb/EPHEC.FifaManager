2ff5525d9ecf07e6eada8efa62522a5023e6bce0
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-04-24 15:08:45 +0200

    suppression FN_CheckGnrClndr

--- BackEnd_DAL/CalendrierData.cs
+++ BackEnd_DAL/CalendrierData.cs
@@ -8,10 +8,5 @@ namespace BackEnd_DAL
 {
     class CalendrierData
     {
-        public string FN_CheckGnrClndrPossible(int champ_id)
-        {
-            Fifa_ManagerEntities ctx = new Fifa_ManagerEntities();
-            return ctx.FN_CheckGnrClndrPossible(champ_id);
-        }
     }
 }
