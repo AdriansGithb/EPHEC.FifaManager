8cc426ec1e92715fdd5b21bd76648916d4397da9
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-07-05 10:30:30 +0200

    Models : +MdlTypeEvent

---
+++ Models/MdlTypeEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class MdlTypeEvent
+    {
+        public int Id { get; set; }
+        public string Libelle { get; set; }
+
+        public int Nb_Jours_Suspension { get; set; }
+        public bool Susp_Next_Match { get; set; }
+
+    }
+}
